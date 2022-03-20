d11d4639fbd2f777e29058469ebf7a561bbf3732
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-03-19 23:04:04 -0300

    done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bp = require("body-parser");
 const ejs = require("ejs");
 const { redirect } = require("express/lib/response");
 const { lt } = require("lodash");
+const _ = require("lodash");
 
 const app = express();
 
@@ -16,16 +17,7 @@ const aboutContent =
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [
-    {
-        title: "example 01",
-        text: "eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu",
-    },
-    {
-        title: "example02",
-        text: "eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu",
-    },
-];
+const posts = [];
 
 app.use(express.static(__dirname + "/public"));
 app.use(bp.urlencoded({ extended: true }));
@@ -75,13 +67,14 @@ app.get("/post", (req, res) => {
     res.redirect("/");
 });
 
-app.get("/post/:title.:subtitle", (req, res) => {
+app.get("/post/:title-:subtitle", (req, res) => {
     let title, content;
-    console.log("i'm here! COMPOSED");
     posts.forEach((element) => {
         if (
-            element.title.toLowerCase() ===
-            `${req.params.title.toLowerCase()} ${req.params.subtitle.toLowerCase()}`
+            _.lowerCase(element.title) ===
+            `${_.lowerCase(req.params.title)} ${_.lowerCase(
+                req.params.subtitle
+            )}`
         ) {
             title = req.params.title + " " + req.params.subtitle;
             content = element.text;
@@ -94,13 +87,12 @@ app.get("/post/:title", (req, res) => {
     let title, content;
     // Imma be honest, using index >>>
     posts.forEach((element) => {
-        if (element.title.toLowerCase() === req.params.title.toLowerCase()) {
+        if (_.lowerCase(element.title) === _.lowerCase(req.params.title)) {
             title = req.params.title;
             content = element.text;
         }
     });
 
-    console.log("i'm here! NORMAL");
     res.render("post", { title: title, content: content });
 });
 
