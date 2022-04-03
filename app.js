4e8aa4c5506123eeb4b62f8823dd73adc33795fa
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-04-03 00:48:40 -0300

    some updates

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ const ejs = require("ejs");
 const { redirect } = require("express/lib/response");
 const { lt } = require("lodash");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/dailyjornalDB");
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+});
+const postModel = mongoose.model("post", postSchema);
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -17,21 +32,25 @@ const aboutContent =
 const contactContent =
     "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
 app.use(express.static(__dirname + "/public"));
 app.use(bp.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     let title, content;
-    content = {
-        mainText: homeStartingContent,
-        array: posts,
-    };
-    title = "Home";
-
-    res.render(title.toLowerCase(), { content: content, title: title });
+    // const posts = [];
+    postModel.find({}, { title: 1, text: 1, _id: 1 }, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        content = {
+            mainText: homeStartingContent,
+            array: result,
+        };
+        title = "Home";
+        res.render(title.toLowerCase(), { content: content, title: title });
+    });
 });
 
 app.get("/about", (req, res) => {
@@ -54,47 +73,62 @@ app.get("/compose", (req, res) => {
     res.render("compose", { title: "Compose" });
 });
 
-// No auth lol
 app.post("/compose", (req, res) => {
-    posts.push({
-        title: req.body.title,
+    // posts.push({
+    //     title: req.body.title,
+    //     text: req.body.text,
+    // });
+    const temp = new postModel({
+        title: _.lowerCase(req.body.title),
         text: req.body.text,
     });
-    res.redirect("/");
+
+    temp.save((err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        res.redirect("/");
+    });
 });
 
 app.get("/post", (req, res) => {
     res.redirect("/");
 });
 
-app.get("/post/:title-:subtitle", (req, res) => {
-    let title, content;
-    posts.forEach((element) => {
-        if (
-            _.lowerCase(element.title) ===
-            `${_.lowerCase(req.params.title)} ${_.lowerCase(
-                req.params.subtitle
-            )}`
-        ) {
-            title = req.params.title + " " + req.params.subtitle;
-            content = element.text;
-        }
-    });
-
-    res.render("post", { title: title, content: content });
-});
+// app.get("/post/:title-:subtitle", (req, res) => {
+//     let title, content;
+//     postModel.find(
+//         {
+//             title: `${_.lowerCase(req.params.title)} ${_.lowerCase(
+//                 req.params.subtitle
+//             )}`,
+//         },
+//         { title: 1, text: 1, _id: 0 },
+//         (err, result) => {
+//             if (err) {
+//                 console.error(err);
+//                 return;
+//             }
+//             title = result[0].title;
+//             content = result[0].text;
+//             res.render("post", { title: title, content: content });
+//         }
+//     );
+// });
 
 app.get("/post/:title", (req, res) => {
     let title, content;
-    // Imma be honest, using index >>>
-    posts.forEach((element) => {
-        if (_.lowerCase(element.title) === _.lowerCase(req.params.title)) {
-            title = req.params.title;
-            content = element.text;
+    postModel.find(
+        { _id: req.params.title },
+        { title: 1, text: 1, _id: 1 },
+        (err, result) => {
+            title = result[0].title;
+            content = result[0].text;
+            if (!err) res.render("post", { title: title, content: content });
         }
-    });
-
-    res.render("post", { title: title, content: content });
+    );
 });
 
 app.listen(process.env.PORT || 3000, () =>
